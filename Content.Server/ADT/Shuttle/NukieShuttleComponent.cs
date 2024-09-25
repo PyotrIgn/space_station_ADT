3664be5bb6e51687f78ab337d0a9a49ba6d50aaf
Eugeny <klochckov.wot@yandex.ru>
2024-09-25 05:46:20 +0400

    тест консоли управления шаттлом

---
+++ Content.Server/ADT/Shuttle/NukieShuttleComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.ADT.Shuttle;
+
+/// <summary>
+/// Добавляем компонент шаттла нюкеров для удаленного управления
+/// </summary>
+[RegisterComponent]
+public sealed partial class NukieShuttleComponent : Component
+{
+
+}
